1f7785368c9191b50fb1198c1438fdd438679eed
Diwan Schneider <diwanschneide@yahoo.fr>
2023-07-11 15:17:11 +0200

    fin des bases du server node

--- back/app.js
+++ back/app.js
@@ -2,4 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) =>{
+    console.log('reqête recue !');
+    next();
+});
+
+app.use((req, res, next) =>{
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) =>{
+    res.json({message: 'votre requête a bien été recue !'});
+    next();
+});
+
+app.use((req, res) =>{
+    console.log('reponse envoyé avec succès !')
+})
 module.exports = app;
