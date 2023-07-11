b6387f2bb84671a8b04231e76220df244d9f0d1a
Diwan Schneider <diwanschneide@yahoo.fr>
2023-07-11 21:55:35 +0200

    creation des routes get et post de l`application

--- back/app.js
+++ back/app.js
@@ -2,22 +2,105 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) =>{
-    console.log('reqête recue !');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) =>{
-    res.status(201);
+  app.post('/api/books', (req, res, next) =>{
+    console.log(req.body);
+    res.status(201).json({
+        message: 'livre posté !'
+    });
     next();
-})
+  });
 
-app.use((req, res, next) =>{
-    res.json({message: 'votre requête a bien été recue !'});
+  app.post('/api/auth/signup', (req, res, next) =>{
+    console.log(req.body);
+    res.status(201).json({
+        message: 'authentification signup reussi !'
+    });
     next();
-});
+  });
 
-app.use((req, res) =>{
-    console.log('reponse envoyé avec succès !')
-})
+  app.post('/api/auth/login', (req, res, next) =>{
+    console.log(req.body);
+    res.status(201).json({
+        message: 'authentification login reussi !'
+    });
+    next();
+  });
+
+
+  app.post('/api/books/:id/rating', (req, res, next) =>{
+    console.log(req.body);
+    res.status(201).json({
+        message: 'note du livre en question'
+    });
+    next();
+  });
+  
+app.get('/api/books', (req, res, next)=>{
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    next();
+    res.status(200).json(stuff);
+  });
+
+  app.get('/api/books/:id', (req, res, next)=>{
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    next();
+    res.status(200).json(stuff);
+  });
+
+  app.get('/api/books/bestrating', (req, res, next)=>{
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      }
+    ];
+    next();
+    res.status(200).json(stuff);
+  });
+
 module.exports = app;
