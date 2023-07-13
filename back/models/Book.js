ef3ab6d5a32e9c2e06ad3c5a550db597b7995907
Diwan Schneider <diwanschneide@yahoo.fr>
2023-07-13 13:43:39 +0200

    modification sur les models

--- back/models/Book.js
+++ back/models/Book.js
@@ -4,14 +4,14 @@ const bookSchema = mongoose.Schema({
     userId: { type: String, required: true},
     title: { type: String, required: true},
     author: { type: String, required: true},
-    imageUrl: { type: String, required: true},
+    imageUrl: { type: String, },
     year: { type: Number, required: true},
     genre: { type: String, required: true},
     ratings:[{
-        userId: { type: String, required: true},
-        grade: { type: Number, required: true},
+        userId: { type: String, },
+        grade: { type: Number, },
     }],
-    averageRating: { type: Number, required: true}
+    averageRating: { type: Number, }
 });
 
 module.exports = mongoose.model('Book', bookSchema);
--- back/routes/stuff.js
+++ back/routes/stuff.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const stuffCtrl = require ('../controllers/stuff');
 
 router.post('/books', stuffCtrl.createBook);
-
 router.get('/books', stuffCtrl.getAllBooks);
-
 router.get('/books/:id', stuffCtrl.getOneBook);
 
 
-  module.exports = router;
+module.exports = router;
