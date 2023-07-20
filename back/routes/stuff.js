8dc28bcd23820c357cdebcf6632b96c85eb7ba68
Diwan Schneider <diwanschneide@yahoo.fr>
2023-07-20 13:05:24 +0200

    ajout de auth aux routes deja existantes qui le necessitaient

--- back/routes/stuff.js
+++ back/routes/stuff.js
@@ -1,9 +1,10 @@
 const express = require ('express');
 const router = express.Router();
+const auth = require ('../middleware/auth');
 
 const stuffCtrl = require ('../controllers/stuff');
 
-router.post('/', stuffCtrl.createBook);
+router.post('/', auth, stuffCtrl.createBook);
 router.get('/', stuffCtrl.getAllBooks);
 router.get('/:id', stuffCtrl.getOneBook);
 
